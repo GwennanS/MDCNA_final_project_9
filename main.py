3a11a6bbbe41a66a46182997deac98839640bbf3
celmoussaoui <C.ElMoussaoui@student.tudelft.nl>
2021-04-13 18:07:28 +0200

    Add sentiment temporal motifs

--- main.py
+++ main.py
@@ -7,18 +7,6 @@ from datetime import datetime
 import PySimpleGUI as sg
 
 
-# TODO: Take the frequency of a sentiment motif (- or +) over a strict period of time. So it is not allowed for a
-#  frequency motif to take more than x timestamps.
-#  Plot motif distribution for delta:
-#  - 1 hour
-#  - 12 hours
-#  - 1 day
-#  - 2 days
-#  - 7 days
-#  - infinite
-#  Compare the distributions and come up with a logical explanation for the differences, if any.
-
-
 def temp_motifs(df):
     print("Retrieving timestamps...")
     timestamps_data = df['TIMESTAMP']
@@ -30,13 +18,16 @@ def temp_motifs(df):
 
     start_date = min(date_times)
     end_date = max(date_times)
+    print("Start date:", start_date)
+    print("End date:", end_date)
 
     print("Transform timestamps to hour differences...")
     hour_differences = []
     for timestamp in date_times:
-        time_diff = abs((timestamp - start_date).days) * 24
+        time_diff = abs((timestamp - start_date).seconds) // 3600 + abs((timestamp - start_date).days) * 24
         hour_differences.append(time_diff)
-    print("Biggest difference is", abs((end_date - start_date).days) * 24, "hours")
+    diff = abs((end_date - start_date).seconds) // 3600 + abs((end_date - start_date).days) * 24
+    print("Biggest difference is", diff, "hours")
 
     print("Building new DataFrame...")
     del df['TIMESTAMP']
@@ -45,20 +36,23 @@ def temp_motifs(df):
     print(df)
 
     print("Create event graphs...")
-    # deltas = [1, 12, 24, 48, 7 * 24, abs((end_date - start_date).days) * 24]
-    EG = EventGraph.from_pandas_eventlist(df, graph_rules='teg')
-    EG.event_graph_rules['delta_cutoff'] = 1
-    EG.build(verbose=True)
+    deltas = [1, 48, abs((end_date - start_date).days) * 24]
+    for i in deltas:
+        print("Delta =", i)
+        EG = EventGraph.from_pandas_eventlist(df, graph_rules='teg')
+        EG.event_graph_rules['delta_cutoff'] = i
+        EG.build(verbose=True)
 
-    print("Calculate edge motifs...")
-    EG.calculate_edge_motifs(edge_type='type', condensed=False)
-    motif_distribution = calculate_motif_distribution(EG)
+        print("Calculate edge motifs...")
+        EG.calculate_edge_motifs(edge_type='type', condensed=False)
+        motif_distribution = calculate_motif_distribution(EG)
 
-    print("Motif distribution: \n", motif_distribution, '\n')
-    print("Plotting results...")
-    motif_distribution.plot(kind='bar', ylim=(0, 0.5))
-    plt.title("Delta 1 hour")
-    plt.show()
+        print("Motif distribution: \n", motif_distribution, '\n')
+        print("Plotting results...")
+        motif_distribution.plot(kind='bar', ylim=(0, 0.5))
+        delta = str(i)
+        plt.title("Delta " + delta + " hours (-)")
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -68,6 +62,8 @@ if __name__ == "__main__":
     body_data = pd.read_csv("data/soc-redditHyperlinks-body.tsv", sep='\t')
     title_data = pd.read_csv("data/soc-redditHyperlinks-title.tsv", sep='\t')
     df = body_data.append(title_data, ignore_index=True)
+
+    # df = df[df['LINK_SENTIMENT'] == -1]
 
     print("Removing unused columns...")
     del df['PROPERTIES']
